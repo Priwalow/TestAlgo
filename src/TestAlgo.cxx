fb536dbc9ff6ee621ab8044e95825d256203ef8a
Sergey Privalov <privalov@rokk1m-lsrp>
2018-07-13 12:03:02 +0700

    Editing gg selection

--- src/TestAlgo.cxx
+++ src/TestAlgo.cxx
@@ -135,8 +135,8 @@ StatusCode TestAlgo::execute()
     return StatusCode::SUCCESS;
   }
 
-  if(evtRecEvent->totalCharged()!=0 || evtRecEvent->totalNeutral()!=2) return sc;
-
+  if(evtRecEvent->totalCharged()!=0) return sc;
+  cout << "Total neutral" << evtRecEvent->totalNeutral() << endl;
   SelectionConfig cfg;
   cfg.EMC_ENDCUP_MIN_COS_THETA=0.86;
   cfg.EMC_ENDCUP_MAX_COS_THETA=0.92;
@@ -146,7 +146,7 @@ StatusCode TestAlgo::execute()
 
   std::list<EvtRecTrack*> nGood = createGoodNeutralTrackList(cfg,evtRecEvent,evtRecTrkCol);
   if(nGood.size()!=2) return sc;
-
+  
   fEvent.run = eventHeader->runNumber();
   fEvent.event = eventHeader->eventNumber();
   fEvent.time = eventHeader->time();
